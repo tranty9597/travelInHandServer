7a20d12e987ead7564424bf847ca2d7a82227c04
Ty <ty.tv01@gmail.com>
2018-10-23 10:12:33 +0700

    nit

--- src/models/apiModel/UserResult/index.js
+++ src/models/apiModel/UserResult/index.js
@@ -1,9 +1,9 @@
-import  Result from "../Result";
+import Result from "../Result";
 
-export default class User extends Result{
+export default class User extends Result {
     constructor(resultCode, message, isError, user, accessToken) {
         super(resultCode, message, isError);
-        this.user = user;
-        this.accessToken = accessToken
+        this.user = { ...user, accessToken };
+
     }
 }
